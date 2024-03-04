a39f0fbc5ad78ed3240a0a7217dc09ec50cd6b10
Vanessa Arellano <vanessaserna60@gmail.com>
2024-03-03 21:33:03 -0600

    delete main call

--- model/svm_model.py
+++ model/svm_model.py
@@ -10,19 +10,6 @@ import numpy as np
 from flask import jsonify
 
 
-def matrix_conf(y_test, y_pred):
-
-    y_test_array = np.array(y_test)
-    confusion_matrix = metrics.confusion_matrix(y_test_array,y_pred)
-
-    # presentar claramente la matriz de confusion  
-    # para eso la convertimos a tabla
-    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [False, True])
-
-    # Desplegamos la matriz de confusión
-    cm_display.plot()
-    plt.show()
-
 def main():
     df_data = convert_json_to_pd.Convert2DF()
     
@@ -31,11 +18,6 @@ def main():
 
     # Dividimos el conjunto dew datos en entrenamiento y prueba 
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=40)
-
-    # Paso 3: Preprocesamiento de datos (escalar características)
-    # scaler = StandardScaler()
-    # X_train = scaler.fit_transform(X_train)
-    # X_test = scaler.transform(X_test)
 
     svm_model = SVC(kernel='linear', random_state=42)  # Utilizamos un kernel lineal en este ejemplo
     svm_model.fit(X_train, y_train)
@@ -49,4 +31,3 @@ def main():
 
     return jsonify(y_pred)
     
-main()
